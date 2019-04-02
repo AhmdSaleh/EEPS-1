cf1d55f269cfb636a74a1444737ac05b068b9e5a
Ahmed-Yasser-A <33327225+Ahmed-Yasser-A@users.noreply.github.com>
2019-04-02 14:42:02 +0200

    Update Trainee.java

--- EEPS/src/Trainee.java
+++ EEPS/src/Trainee.java
@@ -15,6 +15,135 @@ public class Trainee extends Employee{
         this.academicYear=academicYear;
 
     }
+    
+    private void addTotext (String name , int id , int age , String universityName , double GPA , int academicYear) throws IOException {
+
+        File file = new File("Trainees.txt");
+        FileWriter fw = new FileWriter(file , true);
+        PrintWriter pw = new PrintWriter(fw);
+
+        pw.print(id+"@");
+        pw.print(String.format("%s@", name));
+        pw.print(String.format("%s@", age));
+        pw.print(String.format("%s@", salary));
+        pw.print(String.format("%s@", universityName));
+        pw.print(String.format("%s@", GPA));
+        pw.print(String.format("%s@", academicYear));
+
+        pw.close();
+    }
+    
+    public void editTrainee(String removeID , String edname , String edage , String edsalary, String eduniversityName, String edGPA, String edacademicYear){
+        String temp = "temp.txt";
+        String filepath = "Trainees.txt";
+
+        File oldFile = new File (filepath);
+        File newFile = new File (temp);
+        String tempId =" " ;
+        String tempName = " " ;
+        String tempAge = " " ;
+        String tempSalary = " ";
+        String tempUniversityName=" ";
+        String tempGPA = " ";
+        String tempAcademicYear=" " ;
+
+        try {
+
+            FileWriter fw = new FileWriter(temp,true);
+            BufferedWriter bw = new BufferedWriter (fw);
+            PrintWriter pw = new PrintWriter (bw) ;
+
+            Scanner x = new Scanner(new File (filepath));
+            x.useDelimiter("[@\n]");
+
+            while(x.hasNext()){
+
+                tempId=x.next();
+                tempName = x.next();
+                tempAge=x.next();
+                tempSalary=x.next();
+                tempUniversityName=x.next();
+                tempGPA=x.next();
+                tempAcademicYear=x.next();
+
+                if (!removeID.equals(tempId)){
+                    pw.println(tempId+"@"+tempName+"@"+tempAge+"@"+tempSalary+"@"+tempUniversityName+"@"+tempGPA+"@"+tempAcademicYear);
+                }
+                else
+                {
+                    pw.println(tempId+"@"+tempName+"@"+tempAge+"@"+edsalary+"@"+eduniversityName+"@"+edGPA+"@"+edacademicYear);
+                }
+            }
+
+            x.close();
+            pw.flush();
+            pw.close();
+            oldFile.delete();
+            File free = new File (filepath);
+            newFile.renameTo(free);
+
+        }
+        catch (Exception e)
+        {
+
+            System.out.println("There is an Exception here !");
+
+        }
+    }
+    
+    public void deleteTrainee(String removeID){
+        String temp = "temp.txt";
+        String filepath = "Trainees.txt";
+        
+        String tempId =" " ;
+        String tempName = " " ;
+        String tempAge = " " ;
+        String tempSalary = " ";
+        String tempUniversityName=" ";
+        String tempGPA = " ";
+        String tempAcademicYear=" " ;
+
+        File oldFile = new File (filepath);
+        File newFile = new File (temp);
+
+        try {
+
+            FileWriter fw = new FileWriter(temp,true);
+            BufferedWriter bw = new BufferedWriter (fw);
+            PrintWriter pw = new PrintWriter (bw) ;
+
+            Scanner x = new Scanner(new File (filepath));
+            x.useDelimiter("[@\n]");
+
+            while(x.hasNext()){
+
+                tempId=x.next();
+                tempName = x.next();
+                tempAge=x.next();
+                tempSalary=x.next();
+                tempUniversityName=x.next();
+                tempGPA=x.next();
+                tempAcademicYear=x.next();
+
+                if (!removeID.equals(tempId)){
+                    pw.println(tempId+"@"+tempName+"@"+tempAge+"@"+tempSalary+"@"+tempUniversityName+"@"+tempGPA+"@"+tempAcademicYear);
+                }
+            }
+
+            x.close();
+            pw.flush();
+            pw.close();
+            oldFile.delete();
+            File free = new File (filepath);
+            newFile.renameTo(free);
+
+        }
+        catch (Exception e)
+        {
+            System.out.println("There is an Exception here !");
+
+        }
+    }
 
     public double getGPA() {
         return GPA;
